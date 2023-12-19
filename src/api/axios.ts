40c0d902a5145a0c6d6c7219fb6095099bf97294
Ankur Magan <maganankur2007@yahoo.com>
2023-12-19 03:12:06 -0500

    fix: remove api key

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 import qs from 'qs';
 
-const API_KEY = '8ee0fe1fba333c335fdf789d59feaf33';
+const API_KEY = 'Add your API key here';
 
 export const axios = Axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
